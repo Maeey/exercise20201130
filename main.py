f4103336c1a6a5ce86217809049b6e6d40daab4e
Yu <740334290@qq.com>
2020-11-30 14:48:45 +0800

    三次提交

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("first")
     print("second")
-    print("second")
+    print("third")
 
 
 # Press the green button in the gutter to run the script.
